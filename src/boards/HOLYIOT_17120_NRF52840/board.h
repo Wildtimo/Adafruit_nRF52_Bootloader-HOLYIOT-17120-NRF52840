427d24142798c6994bf70921d7e4a92f9b8378d1
Wildtimo <114223553+Wildtimo@users.noreply.github.com>
2026-07-29 13:03:23 -0400

    Update board.h

--- src/boards/HOLYIOT_17120_NRF52840/board.h
+++ src/boards/HOLYIOT_17120_NRF52840/board.h
@@ -9,19 +9,6 @@
 #define LED_PRIMARY_PIN   PINNUM(0, 4)
 #define LED_STATE_ON      1
 
-
-/*------------------------------------------------------------------*/
-/* BUTTON
- *------------------------------------------------------------------*/
-
-// No buttons on this board
-// DFU is entered by USB bootloader timeout or software reset
-
-#define BUTTON_DFU     PINNUM(1, 02)
-#define BUTTON_DFU_OTA PINNUM(0, 10)
-#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
-
-
 //--------------------------------------------------------------------+
 // BLE OTA
 //--------------------------------------------------------------------+
@@ -33,10 +20,9 @@
 //--------------------------------------------------------------------+
 // USB
 //--------------------------------------------------------------------+
-
-#define USB_DESC_VID           0x239A
-#define USB_DESC_UF2_PID       0x0029
-#define USB_DESC_CDC_ONLY_PID  0x002A
+#define USB_DESC_VID          0x239A
+#define USB_DESC_UF2_PID      0x00B3
+#define USB_DESC_CDC_ONLY_PID 0x00B3
 
 #define UF2_PRODUCT_NAME      "HOLYIOT-17120 nRF52840"
 #define UF2_VOLUME_LABEL      "HLYT840BOOT"
