016218879e37262a3589ca112628223fc15138f9
Wildtimo <114223553+Wildtimo@users.noreply.github.com>
2026-07-29 08:31:45 -0400

    fix

--- src/boards/HOLYIOT_17120_NRF52840/board.h
+++ src/boards/HOLYIOT_17120_NRF52840/board.h
@@ -1,16 +1,49 @@
-#pragma once
+#ifndef HOLYIOT_17120_H
+#define HOLYIOT_17120_H
 
-#define LEDS_NUMBER         1
-#define LED_PRIMARY_PIN     NRF_GPIO_PIN_MAP(1,1)
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
 
-#define LED_STATE_ON        1
-#define LED_STATE_OFF       0
+#define LEDS_NUMBER       1
+#define LED_PRIMARY_PIN   33      // P1.01 = (1*32)+1
+#define LED_STATE_ON      1
 
-#define BUTTONS_NUMBER      0
+// No secondary LED
+#define LED_SECONDARY_PIN 0xFF
 
-#define DFU_LED             LED_PRIMARY_PIN
 
-#define UART_TX_PIN         0xFF
-#define UART_RX_PIN         0xFF
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
 
-#define PIN_NEOPIXEL        0xFF
+// No buttons on this board
+// DFU is entered by USB bootloader timeout or software reset
+
+#define BUTTON_DFU        0xFF
+#define BUTTON_DFU_OTA    0xFF
+#define BUTTON_PULL       NRF_GPIO_PIN_PULLUP
+
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+
+#define BLEDIS_MANUFACTURER "HOLYIOT"
+#define BLEDIS_MODEL        "17120 NRF52840"
+
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+
+#define USB_DESC_VID          0x239A
+#define USB_DESC_UF2_PID      0x00DA
+#define USB_DESC_CDC_ONLY_PID 0x00DA
+
+#define UF2_PRODUCT_NAME      "HOLYIOT-17120 nRF52840"
+#define UF2_BOARD_ID          "HOLYIOT-17120-nRF52840-v1"
+#define UF2_INDEX_URL         "https://www.holyiot.com"
+
+
+#endif // HOLYIOT_17120_H
--- src/boards/HOLYIOT_17120_NRF52840/pinconfig.c
+++ src/boards/HOLYIOT_17120_NRF52840/pinconfig.c
@@ -1,12 +1,18 @@
-board_init();
+#include "boards.h"
+#include "uf2/configkeys.h"
 
-led_blink(100);
-
-tusb_init();
-
-board_delay(3000);
-
-if (!usb_activity)
+__attribute__((used, section(".bootloaderConfig")))
+const uint32_t bootloaderConfig[] =
 {
-    bootloader_app_start();
-}
+  CFG_MAGIC0, CFG_MAGIC1,
+
+  5, 100,
+
+  204, 0x100000,                                // FLASH_BYTES
+  205, 0x40000,                                 // RAM_BYTES
+  208, (USB_DESC_VID << 16) | USB_DESC_UF2_PID,
+  209, 0xada52840,                              // nRF52840 UF2 family
+  210, 0x20,                                    // 32 GPIO pins
+
+  0,0,0,0,0,0,0,0
+};
